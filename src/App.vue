5392598083656af4963f065823da1045e2ce14e1
Jason-momo <8185668@qq.com>
2021-08-10 21:34:31 +0800

    'update'

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <MainTabBar></MainTabBar>
+  </div>
+  <router-view/>
+</template>
+<script>
+import MainTabBar from "@/components/component/mainTabBar/MainTabBar";
+export default {
+  name: "App",
+  components: {
+    MainTabBar
+  }
+}
+</script>
+<style>
+</style>
