68c3f13473610e3029b5d89906bb4af8edbc7acf
Jason-momo <8185668@qq.com>
2021-09-02 14:28:58 +0800

    update:actions

--- src/store/actions.js
+++ src/store/actions.js
@@ -10,7 +10,5 @@ const actions = {
     async getHomeBanners() {
         return await ajax('https://mock.apipost.cn/app/mock/project/f1f6c282-67b1-445f-a451-7fbc3dc849ad/home/banners', 'GET')
     },
-
-
 }
 export default actions
