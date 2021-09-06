859e27ec8e42bda7fa5da010e2c366815dc10fa2
Jason-momo <8185668@qq.com>
2021-09-06 10:45:27 +0800

    add better-scroll

---
+++ src/components/common/scroll/Scroll.vue
@@ -0,0 +1,23 @@
+<template>
+
+</template>
+
+<script>
+import BetterScroll from "better-scroll"
+
+export default {
+  name: "Scroll",
+  data() {
+    return {
+      scroll: null,
+      message: "haha"
+    }
+  },
+  mounted() {
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
